7777f830aa971822d69fb2227e88ea44cec34aed
aruki <Joeld1027@gmail.com>
2020-04-21 21:32:32 -0500

    updated shopdata group items in cart

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,10 +4,11 @@ import './custom-button.styles.scss';
 const CustomButton = ({
 	children,
 	isGoogleSignIn,
+	inverted,
 	...otherProps
 }) => (
 	<button
-		className={`${
+		className={`${inverted ? 'inverted' : ''} ${
 			isGoogleSignIn ? 'google-sign-in' : ''
 		} custom-button`}
 		{...otherProps}
