5262764cf6219f3f90135f476deab639049763a4
aruki <Joeld1027@gmail.com>
2020-04-24 19:15:22 -0500

    added code that pulls in the shop data and stores it to the store reducer from the shop page, added spinner

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
@@ -8,34 +7,32 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/original.svg';
-import './header.styles.scss';
+import {
+	HeaderContainer,
+	OptionLink,
+	OptionsContainer,
+	LogoContainer,
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-	<div className='header'>
-		<Link className='logo-container' to='/'>
+	<HeaderContainer>
+		<LogoContainer to='/'>
 			<Logo className='logo' />
-		</Link>
-		<div className='options'>
-			<Link className='option' to='/shop'>
-				SHOP
-			</Link>
-			<Link className='option' to='/shop'>
-				CONTACT
-			</Link>
+		</LogoContainer>
+		<OptionsContainer>
+			<OptionLink to='/shop'>SHOP</OptionLink>
+			<OptionLink to='/shop'>CONTACT</OptionLink>
 			{currentUser ? (
-				<div className='option' onClick={() => auth.signOut()}>
-					{' '}
+				<OptionLink as='div' onClick={() => auth.signOut()}>
 					SIGN OUT
-				</div>
+				</OptionLink>
 			) : (
-				<Link className='opton' to='/signin'>
-					SIGN IN
-				</Link>
+				<OptionLink to='/signin'>SIGN IN</OptionLink>
 			)}
 			<CartIcon />
-		</div>
+		</OptionsContainer>
 		{hidden ? null : <CartDropdown />}
-	</div>
+	</HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import CollectionPage from '../collection/collection.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
+import {
+	firestore,
+	convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-const ShopPage = ({ match }) => (
-	<div>
-		<Route
-			exact
-			path={`${match.path}`}
-			component={CollectionsOverview}
-		/>
-		<Route
-			path={`${match.path}/:collectionId`}
-			component={CollectionPage}
-		/>
-	</div>
+const CollectionsOverviewWithSpinner = WithSpinner(
+	CollectionsOverview
 );
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
-export default ShopPage;
+class ShopPage extends Component {
+	state = {
+		loading: true,
+	};
+
+	unsubscribrFromSnapshot = null;
+
+	componentDidMount() {
+		const { updateCollections } = this.props;
+		const collectionRef = firestore.collection('collections');
+		collectionRef.get().then(async (snapshot) => {
+			const collectionsMap = convertCollectionsSnapshotToMap(
+				snapshot
+			);
+			updateCollections(collectionsMap);
+			this.setState({ loading: false });
+		});
+	}
+
+	render() {
+		const { match } = this.props;
+		const { loading } = this.state;
+		return (
+			<div>
+				<Route
+					exact
+					path={`${match.path}`}
+					render={(props) => (
+						<CollectionsOverviewWithSpinner
+							isLoading={loading}
+							{...props}
+						/>
+					)}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					render={(props) => (
+						<CollectionPageWithSpinner
+							isLoading={loading}
+							{...props}
+						/>
+					)}
+				/>
+			</div>
+		);
+	}
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	updateCollections: (collectionsMap) =>
+		dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
